85d1fd7f921059704348c86173c7bfcdfd5cd9a5
Ixtlaxt <bmcgrath@iinet.net.au>
2019-09-01 22:12:19 +0200

    Some cleaning

--- src/main/java/net/grallarius/sunderedblocks/init/ModBlocks.java
+++ src/main/java/net/grallarius/sunderedblocks/init/ModBlocks.java
@@ -25,39 +25,11 @@ public class ModBlocks {
     public static final List<Block> BLOCKS = new ArrayList<>();
     public static final List<Item> ITEMBLOCKS = new ArrayList<>();
 
-    public static final Block test_block = null;
-    public static final BlockVertslab test_vertslab = null;
-
-    public static final SandBlock sand_black_block = null;
-    public static final SandBlock sand_brown_block = null;
-    public static final SandBlock sand_grey_block = null;
-    public static final SandBlock sand_tropic_block = null;
-    public static final SandBlock sand_white_block = null;
-
-    public static final Block stone_red_dark_raw_block = register("stone_red_dark_raw_block", new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.2f, 3.2f)));
-    public static final Block stone_red_dark_brick_block = register("stone_red_dark_brick_block", new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0f, 3.0f)));
-    public static final Block stone_red_dark_tile_block = register("stone_red_dark_tile_block", new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0f, 3.0f)));
-    public static final Block stone_red_dark_cut_block = register("stone_red_dark_cut_block", new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0f, 3.0f)));
-
-    public static final BlockModStairs stone_red_dark_raw_stairs = null;
-    public static final BlockModStairs stone_red_dark_brick_stairs = null;
-    public static final BlockModStairs stone_red_dark_tile_stairs = null;
-    public static final BlockModStairs stone_red_dark_cut_stairs = null;
-
-    public static final BlockVertslab stone_red_dark_raw_vertslab = null;
-    public static final BlockVertslab stone_red_dark_brick_vertslab = null;
-    public static final BlockVertslab stone_red_dark_tile_vertslab = null;
-    public static final BlockVertslab stone_red_dark_cut_vertslab = null;
-
-    public static final SlabBlock stone_red_dark_raw_slab = null;
-    public static final SlabBlock stone_red_dark_brick_slab = null;
-    public static final SlabBlock stone_red_dark_tile_slab = null;
-    public static final SlabBlock stone_red_dark_cut_slab = null;
-
-    public static final RotatedPillarBlock stone_red_dark_pillar_basic = null;
-
-    public static final BlockModDoor stone_red_dark_door = null;
-    public static final BlockModTrapDoor stone_red_dark_trapdoor = null;
+    public static final Block sand_black_block = register("sand_black_block", new SandBlock(14406560, Block.Properties.create(Material.SAND).hardnessAndResistance(0.5f, 0.5f).sound(SoundType.SAND)));
+    public static final Block sand_brown_block = register("sand_brown_block", new SandBlock(14406560, Block.Properties.create(Material.SAND).hardnessAndResistance(0.5f, 0.5f).sound(SoundType.SAND)));
+    public static final Block sand_grey_block = register("sand_grey_block", new SandBlock(14406560, Block.Properties.create(Material.SAND).hardnessAndResistance(0.5f, 0.5f).sound(SoundType.SAND)));
+    public static final Block sand_tropic_block = register("sand_tropic_block", new SandBlock(14406560, Block.Properties.create(Material.SAND).hardnessAndResistance(0.5f, 0.5f).sound(SoundType.SAND)));
+    public static final Block sand_white_block = register("sand_white_block", new SandBlock(14406560, Block.Properties.create(Material.SAND).hardnessAndResistance(0.5f, 0.5f).sound(SoundType.SAND)));
 
     public static Block register(String name, Block block) {
         return register(name, block, new Item.Properties().group(creativeTab));
@@ -96,30 +68,6 @@ public class ModBlocks {
         BLOCKS.forEach(block -> event.getRegistry().register(block));
         BLOCKS.clear();
 
-        event.getRegistry().registerAll(
-                new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0f, 3.0f)).setRegistryName(SunderedBlocks.MODID, "test_block"),
-                new BlockModStairs(stone_red_dark_raw_block.getDefaultState(), Block.Properties.from(stone_red_dark_raw_block)).setRegistryName(SunderedBlocks.MODID, "stone_red_dark_raw_stairs"),
-                new BlockModStairs(stone_red_dark_brick_block.getDefaultState(), Block.Properties.from(stone_red_dark_brick_block)).setRegistryName(SunderedBlocks.MODID, "stone_red_dark_brick_stairs"),
-                new BlockModStairs(stone_red_dark_tile_block.getDefaultState(), Block.Properties.from(stone_red_dark_tile_block)).setRegistryName(SunderedBlocks.MODID, "stone_red_dark_tile_stairs"),
-                new BlockModStairs(stone_red_dark_cut_block.getDefaultState(), Block.Properties.from(stone_red_dark_cut_block)).setRegistryName(SunderedBlocks.MODID, "stone_red_dark_cut_stairs"),
-                new BlockVertslab(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0f, 3.0f)).setRegistryName(SunderedBlocks.MODID, "test_vertslab"),
-                new BlockVertslab(Block.Properties.from(stone_red_dark_raw_block)).setRegistryName(SunderedBlocks.MODID, "stone_red_dark_raw_vertslab"),
-                new BlockVertslab(Block.Properties.from(stone_red_dark_brick_block)).setRegistryName(SunderedBlocks.MODID, "stone_red_dark_brick_vertslab"),
-                new BlockVertslab(Block.Properties.from(stone_red_dark_tile_block)).setRegistryName(SunderedBlocks.MODID, "stone_red_dark_tile_vertslab"),
-                new BlockVertslab(Block.Properties.from(stone_red_dark_cut_block)).setRegistryName(SunderedBlocks.MODID, "stone_red_dark_cut_vertslab"),
-                new SlabBlock(Block.Properties.from(stone_red_dark_raw_block)).setRegistryName(SunderedBlocks.MODID, "stone_red_dark_raw_slab"),
-                new SlabBlock(Block.Properties.from(stone_red_dark_brick_block)).setRegistryName(SunderedBlocks.MODID, "stone_red_dark_brick_slab"),
-                new SlabBlock(Block.Properties.from(stone_red_dark_tile_block)).setRegistryName(SunderedBlocks.MODID, "stone_red_dark_tile_slab"),
-                new SlabBlock(Block.Properties.from(stone_red_dark_cut_block)).setRegistryName(SunderedBlocks.MODID, "stone_red_dark_cut_slab"),
-                new RotatedPillarBlock(Block.Properties.from(stone_red_dark_cut_block)).setRegistryName(SunderedBlocks.MODID, "stone_red_dark_pillar_basic"),
-                new BlockModDoor(Block.Properties.from(stone_red_dark_cut_block)).setRegistryName(SunderedBlocks.MODID, "stone_red_dark_door"),
-                new BlockModTrapDoor(Block.Properties.from(stone_red_dark_cut_block)).setRegistryName(SunderedBlocks.MODID, "stone_red_dark_trapdoor"),
-                new SandBlock(14406560, Block.Properties.create(Material.SAND).hardnessAndResistance(0.5f, 0.5f).sound(SoundType.SAND)).setRegistryName(SunderedBlocks.MODID, "sand_black_block"),
-                new SandBlock(14406560, Block.Properties.create(Material.SAND).hardnessAndResistance(0.5f, 0.5f).sound(SoundType.SAND)).setRegistryName(SunderedBlocks.MODID, "sand_brown_block"),
-                new SandBlock(14406560, Block.Properties.create(Material.SAND).hardnessAndResistance(0.5f, 0.5f).sound(SoundType.SAND)).setRegistryName(SunderedBlocks.MODID, "sand_grey_block"),
-                new SandBlock(14406560, Block.Properties.create(Material.SAND).hardnessAndResistance(0.5f, 0.5f).sound(SoundType.SAND)).setRegistryName(SunderedBlocks.MODID, "sand_tropic_block"),
-                new SandBlock(14406560, Block.Properties.create(Material.SAND).hardnessAndResistance(0.5f, 0.5f).sound(SoundType.SAND)).setRegistryName(SunderedBlocks.MODID, "sand_white_block")
-        );
     }
 
     @SubscribeEvent
--- src/main/java/net/grallarius/sunderedblocks/init/ModItems.java
+++ src/main/java/net/grallarius/sunderedblocks/init/ModItems.java
@@ -18,43 +18,9 @@ public class ModItems {
     public static final ItemGroup creativeTab = new ItemGroup("sunderBlocks") {
         @Override
         public ItemStack createIcon() {
-            return new ItemStack(ModBlocks.stone_red_dark_cut_block);
+            return new ItemStack(InitSpartine.stone_spartine_pale_cut_block);
         }
     };
-
-/*    @SubscribeEvent
-    public static void registerItems(RegistryEvent.Register<Item> event){
-        event.getRegistry().registerAll(
-                new Item(new Item.Properties().group(creativeTab)).setRegistryName(SunderedBlocks.MODID, "test_item"),
-                createItemBlock(ModBlocks.test_block, new Item.Properties().group(creativeTab).maxStackSize(64)),
-                //createItemBlock(ModBlocks.stone_red_dark_raw_block, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_cut_block, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_tile_block, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_brick_block, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_raw_stairs, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_brick_stairs, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_tile_stairs, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_cut_stairs, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.test_vertslab, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_raw_vertslab, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_brick_vertslab, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_tile_vertslab, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_cut_vertslab, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_raw_slab, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_brick_slab, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_tile_slab, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_cut_slab, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_pillar_basic, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_door, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.stone_red_dark_trapdoor, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.sand_black_block, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.sand_brown_block, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.sand_grey_block, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.sand_tropic_block, new Item.Properties().group(creativeTab)),
-                createItemBlock(ModBlocks.sand_white_block, new Item.Properties().group(creativeTab))
-        );
-        //InitSterist.registerItems(event);
-    }*/
 
     public static BlockItem createItemBlock(Block block, Item.Properties properties) {
         System.out.println("CreateItemBlock: " + block.getRegistryName());
